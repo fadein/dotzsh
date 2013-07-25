999056e50a2443f3d7c901902c4cfa18481d289c
Erik on AIX <efalor@spillman.com>
2013-07-25 08:57:01 -0600

    chicken.zsh: port Chicken Scheme to AIX

---
+++ tasks/chicken.zsh
@@ -0,0 +1,25 @@
+#!/usr/local/bin/zsh
+PURPOSE='Build Chicken on AIX'
+VERSION="1.0"
+   DATE="Tue Jul  9 22:51:16 MDT 2013"
+ AUTHOR="Erik Falor <efalor@spillman.com>"
+TASKNAME=$0:t:r
+
+env() {
+	# use gcc 4.4.5, not the old 3.2.2
+	PATH=/home/efalor/.storm-dev/bin:/usr/bin:/etc:/usr/sbin:/usr/ucb:/home/efalor/bin:/usr/bin/X11:/sbin:/home/efalor/.zsh:/home/efalor/scripts
+
+	#export CSC_OPTIONS='-vv -C -maix64 -L -maix64 -Wl,-R"." -Wl,-bsvr4 -Wl,-bbigtoc'
+	#echo "CSC_OPTIONS is exported as a work-around for chicken-install"
+	alias make=make\ PLATFORM=aix\ PREFIX=$HOME/.$(hostname)
+	alias vim=/usr/local/bin/vim
+	alias screen=/usr/local/bin/screen
+	echo "make is aliased to 'make\ PLATFORM=aix\ PREFIX=$HOME/.$(hostname)'"
+	#_TODO=("Figure out why modules built with chicken-install can't be loaded - see build.log files in eggs/base64 and build/chicken.*")
+
+	cd /home/efalor/build/chicken
+}
+
+
+
+source $0:h/__TASKS.zsh
