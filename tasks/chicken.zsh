615ffa81c27a76a0490415be7b9bbf1e916cf5d3
Erik Falor (fadein) <ewfalor@gmail.com>
2013-10-18 11:50:28 -0600

    Linux code in Chicken task

--- tasks/chicken.zsh
+++ tasks/chicken.zsh
@@ -5,27 +5,39 @@ VERSION="1.0"
  AUTHOR="Erik Falor <efalor@spillman.com>"
 TASKNAME=$0:t:r
 
-setup() {
-	if [[ `uname` != 'AIX' ]]; then
-		die "This task is for AIX"
-	fi
-}
+case `uname` in
+	AIX)
+		env() {
+			# use gcc 4.4.5, not the old 3.2.2
+			PATH=/home/efalor/.storm-dev/bin:/usr/bin:/etc:/usr/sbin:/usr/ucb:/home/efalor/bin:/usr/bin/X11:/sbin:/home/efalor/.zsh:/home/efalor/scripts
 
-env() {
-	# use gcc 4.4.5, not the old 3.2.2
-	PATH=/home/efalor/.storm-dev/bin:/usr/bin:/etc:/usr/sbin:/usr/ucb:/home/efalor/bin:/usr/bin/X11:/sbin:/home/efalor/.zsh:/home/efalor/scripts
+			#export CSC_OPTIONS='-vv -C -maix64 -L -maix64 -Wl,-R"." -Wl,-bsvr4 -Wl,-bbigtoc'
+			#echo "CSC_OPTIONS is exported as a work-around for chicken-install"
+			alias make=make\ PLATFORM=aix\ PREFIX=$HOME/.$(hostname)
+			alias vim=/usr/local/bin/vim
+			alias screen=/usr/local/bin/screen
+			echo "make is aliased to 'make\ PLATFORM=aix\ PREFIX=$HOME/.$(hostname)'"
 
-	#export CSC_OPTIONS='-vv -C -maix64 -L -maix64 -Wl,-R"." -Wl,-bsvr4 -Wl,-bbigtoc'
-	#echo "CSC_OPTIONS is exported as a work-around for chicken-install"
-	alias make=make\ PLATFORM=aix\ PREFIX=$HOME/.$(hostname)
-	alias vim=/usr/local/bin/vim
-	alias screen=/usr/local/bin/screen
-	echo "make is aliased to 'make\ PLATFORM=aix\ PREFIX=$HOME/.$(hostname)'"
+			persistentTodo /home/efalor/build/chicken/.todo
 
-	persistentTodo /home/efalor/build/chicken/.todo
+			cd /home/efalor/build/chicken
+		}
+		;;
 
-	cd /home/efalor/build/chicken
-}
+	Linux)
+		env() {
+
+			alias make=make\ PLATFORM=linux\ PREFIX=$HOME/.$(hostname)
+			echo "make is aliased to 'make\ PLATFORM=linux\ PREFIX=$HOME/.$(hostname)'"
+
+			persistentTodo ~/chicken/.todo
+
+			gitprompt
+			cd ~/chicken/chicken-core.git/
+		}
+		;;
+esac
+
 
 
 
