ec995c0d12a889f9a9f5913fda080f7f03924552
Erik Falor (fadein) <ewfalor@gmail.com>
2014-09-25 00:21:18 -0600

    use new privileges for usuvpn task

--- tasks/usuvpn.zsh
+++ tasks/usuvpn.zsh
@@ -1,34 +1,68 @@
 #!/bin/zsh
 
 PURPOSE='bring up VPN Tunnel to USU'
-VERSION=1.1
-   DATE="Fri Feb 15 14:37:59 MST 2013"
+VERSION=1.2
+   DATE="Wed Sep 24 23:56:27 MDT 2014"
  AUTHOR="Erik Falor <ewfalor@gmail.com>"
 
-PROGNAME=$0:t
+PROGNAME=$0
 TASKNAME=$0:t:r
 
-SUDO=/usr/bin/sudo
+ROUTE=/bin/route
+IPTABLES=/sbin/iptables
+OPENCONNECT=/usr/sbin/openconnect
 
-# spawn a root shell
+BGPID=
+setup() {
+    raisePrivs
+    echo 'yo@aBkeE/j4Gleys' | $OPENCONNECT -u A00319537 --passwd-on-stdin sslvpn.usu.edu &
+    BGPID=$!
+
+    until grep -q @VPNC_GENERATED@ /etc/resolv.conf; do
+        sleep 2
+    done
+
+    sed -i -e '/resolv.conf.head/ a\
+domain falor \
+nameserver 192.168.1.1
+' /etc/resolv.conf
+}
+
 spawn() {
-	$SUDO TASK=$TASKNAME $ZSH_NAME
+    dropPrivsAndSpawn
 }
 
-env() {
-	_TODO=(
-		'$ for S in /etc/init.d/{ipsec,xl2tpd}; do $S start; done'
-		'$ ipsec auto --up L2TP-PSK'
-		'$ echo "c L2TPserver" > /var/run/xl2tpd/l2tp-control'
-		'$ route add -host 129.123.67.252 gw 192.168.1.1 dev eth0'
-		'$ route add -net 129.123.0.0 netmask 255.255.0.0 dev ppp0'
-		'$ iptables -I INPUT 3 -i ppp0 -j ACCEPT' )
+# env() {
+#     $SUDO $ROUTE add -host 129.123.67.252 gw 192.168.1.1 dev enp2s0
+#     $SUDO $ROUTE add -net 129.123.0.0 netmask 255.255.0.0 dev tun0
+#     $SUDO $IPTABLES -A INPUT -i tun0 -j ACCEPT
+# }
 
-	cd /root/usuvpn
+cleanup() {
+    if [[ -n "$BGPID" ]]; then
+        until kill $!; do
+            print -n .
+            sleep 1
+        done
+    fi
 }
 
-# TODO: disable VPN connection
-#cleanup() { }
+#  # spawn a root shell
+#  spawn() {
+#  	$SUDO TASK=$TASKNAME $ZSH_NAME
+#  }
+#  
+#  env() {
+#      # '$ for S in /etc/init.d/{ipsec,xl2tpd}; do $S start; done'
+#      # '$ ipsec auto --up L2TP-PSK'
+#      # '$ echo "c L2TPserver" > /var/run/xl2tpd/l2tp-control'
+#  	_TODO=(
+#  		'$ route add -host 129.123.67.252 gw 192.168.1.1 dev enp2s0'
+#  		'$ route add -net 129.123.0.0 netmask 255.255.0.0 dev tun0'
+#  		'$ iptables -A INPUT -i tun0 -j ACCEPT' )
+#  
+#  	cd /root/usuvpn
+#  }
 
 source $0:h/__TASKS.zsh
 
