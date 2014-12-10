a284fddda2772d77433a863123c6ac226f8947ed
Erik Falor <ewfalor@gmail.com>
2014-12-10 01:09:31 -0700

    use new privs for spillman vpn task

--- tasks/spillman.zsh
+++ tasks/spillman.zsh
@@ -4,19 +4,23 @@ PURPOSE="Spillman connect via vpnc"
 VERSION="1.0"
    DATE="Wed Jul 31 23:13:08 MDT 2013"
  AUTHOR="Erik Falor <ewfalor@gmail.com>"
-
+PROGNAME=$0
 TASKNAME=$0:t:r
 
-SUDO=/usr/bin/sudo
 VPNC=/usr/sbin/vpnc
 VPNC_D=/usr/sbin/vpnc-disconnect
 
 setup() {
-    $SUDO $VPNC spillman
+    raisePrivs
+    $VPNC spillman
+}
+
+spawn() {
+    dropPrivsAndSpawn $ZSH_NAME
 }
 
 cleanup() {
-    $SUDO $VPNC_D
+    $VPNC_D
 }
 
 source $0:h/__TASKS.zsh
