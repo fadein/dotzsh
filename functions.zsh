89d509094bc93a8e8e71d79a2d0e0b88ddb56273
Erik Falor (fadein) <ewfalor@gmail.com>
2015-11-24 20:55:33 -0700

    Updating for Zsh 5.1.1

--- functions.zsh
+++ functions.zsh
@@ -38,11 +38,8 @@ uniquify() {
 
 # this little gem lets me say .. .. .. to go back three directories
 ..() {
-	#TODO: what were these variables for?
-	local SAVEOLDPWD="$PWD"
-	local SAVESHUSH=$SHUSH
-
-	#join given arguments with /
+	#join given arguments with / and run the builtin "cd" once
+	#so as not to screw up "cd -"
 	local IFS=/
 	#TODO: could the -q argument to cd be useful to keep things quiet here?
 	cd "../$*"
@@ -68,7 +65,7 @@ _dotdotcomp() {
 
 # this hook function decides which commands are added to the shell's history
 zshaddhistory() {
-	[[ "$1" =~ '^(fg|bg)\s+' ]] && return 1
+	[[ "$1" =~ '^(fg|bg)\s+$' ]] && return 1
 	return 0
 }
 
