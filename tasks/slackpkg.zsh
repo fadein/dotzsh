69b0d5c807649aad5190caecb92b96f7ca9b34bf
Erik at School <erik.falor@usu.edu>
2017-07-27 13:09:25 -0600

    add helpful note to slackpkg.zsh

--- tasks/slackpkg.zsh
+++ tasks/slackpkg.zsh
@@ -45,6 +45,14 @@ Run this command to install new packages added since last update:
 
 Run this command when you're ready to upgrade the set of installed packages:
 	\$ $SLACKPKG upgrade-all
+
+
+After updating the kernel on LVM systems (Voyager2, Mariner), you must re-create the initrd
+Run this command, inserting the version number of the new kernel:
+
+	# /usr/share/mkinitrd/mkinitrd_command_generator.sh -k 4.4.75
+
+It should echo back a command including a big list of kernel modules
 MESSAGE
 }
 
