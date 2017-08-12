a6131ca79a2daa9cded94f688377fc11f4517d89
Erik on Apollo <ewfalor@gmail.com>
2017-08-12 11:10:20 -0600

    better heuristic for determining when I'm on my own network

--- tasks/viking.zsh
+++ tasks/viking.zsh
@@ -1,17 +1,20 @@
 #!/bin/zsh
 
-PURPOSE="Log into viking "
-VERSION=0.2
-   DATE="Tue Jul  7 17:49:43 MDT 2015"
+PURPOSE="Log into viking"
+VERSION=0.3
+   DATE="Sat Aug 12 10:59:40 MDT 2017"
  AUTHOR="Erik Falor <ewfalor@gmail.com>"
 
 PROGNAME=$0:t
 TASKNAME=$0:t:r
 
 SSH=/usr/bin/ssh
-HOST=viking2.falor
-
 CMD="exec /home/fadein/scripts/comms.tmux"
+
+HOME_WIFI="get your own internet"
+INTRANET_HOST=viking2.falor
+INTERNET_HOST=viking-dyn
+HOST=$INTRANET_HOST
 
 #spawn our task shell
 spawn() {
@@ -21,10 +24,15 @@ spawn() {
         LANG=C
     fi
 
-    WIFI="$(iwconfig wlan0 | \grep ESSID | cut -d'"' -f2)"
-    if [[ $WIFI != "get your own internet" ]]; then
-        HOST=viking-dyn
-    fi
+    # If I'm not on my home wifi network, connect
+    for WIFI_IFS in /sys/class/net/w*; do
+        WIFI_IFS=$(basename $WIFI_IFS)
+        WIFI="$(iwconfig $WIFI_IFS | \grep ESSID | cut -d'"' -f2)"
+        if [[ $WIFI != $HOME_WIFI ]]; then
+            HOST=$INTERNET_HOST
+            break
+        fi
+    done
 
     TASK=$TASKNAME $SSH -t $HOST "LANG=$LANG $CMD"
 }
@@ -32,4 +40,3 @@ spawn() {
 source $0:h/__TASKS.zsh
 
 # vim:set foldenable foldmethod=indent filetype=sh tabstop=4 expandtab:
-
