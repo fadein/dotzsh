9384613b1f970ebf8c5126a6cd7c53de33b1efa6
Erik on Voyager2 <ewfalor@gmail.com>
2017-10-02 09:57:29 -0600

    slackpkg changes just for voy2; are these good for all Slackboxen?

--- tasks/slackpkg.zsh
+++ tasks/slackpkg.zsh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 
  PURPOSE="Slackware update task"
- VERSION="1.2"
-    DATE="Sun Jul 16 21:18:06 MDT 2017"
+ VERSION="1.3"
+    DATE="Sat Sep 23 15:11:35 MDT 2017"
   AUTHOR="Erik Falor"
 PROGNAME=$0
 TASKNAME=$0:t:r
@@ -21,24 +21,20 @@ spawn() {
 
 env() {
 	$SLACKPKG update
-	$SLACKPKG upgrade-all
 
 	_TODO=(
 		"\$ $SLACKPKG install-new"
 		"\$ $SLACKPKG upgrade-all"
 	)
 
-	# TODO: make this smart - save the latest update timestamp to some file,
-	# and use that for the comparison instead of this janky, hardcoded stamp.
-	# Also, I don't believe that this bit works, anyway.
-	print These new packages have been added to the repo:
-	local last_update="Thu Sep 17 20:15:00 UTC 2015"
-	sed -n -e '/Added.$/p' -e "/^$last_update/q" /var/lib/slackpkg/ChangeLog.txt
+	if [[ -f /tmp/slackpkg.last_update.txt ]]; then
+		local last_update=$(< /tmp/slackpkg.last_update.txt)
+		print These new packages have been added to the repo:
+		sed -n -e '/Added.$/p' -e "/^$last_update/q" /var/lib/slackpkg/ChangeLog.txt
+	fi
 
-	_TODO=(
-		"$ $SLACKPKG install-new"
-		"$ $SLACKPKG upgrade-all"
-	)
+	# Store the date of this update
+	head -n1 /var/lib/slackpkg/ChangeLog.txt > /tmp/slackpkg.last_update.txt
 
 	>&1 <<MESSAGE
 
@@ -57,6 +53,9 @@ Run this command to install new packages added since last update:
 Run this command when you're ready to upgrade the set of installed packages:
 	\$ $SLACKPKG upgrade-all
 
+MESSAGE
+
+IMPORTANT="
 
 !!!!!!!!!!!!!!!!!
 !!! IMPORTANT !!!
@@ -65,15 +64,34 @@ Run this command when you're ready to upgrade the set of installed packages:
 After updating the kernel on LVM systems (Voyager2, Mariner), you must copy the
 new kernel files into /boot/efi/EFI/Slackware/
 
-Make sure that the vmlinuz file there is replaced by the vmlinuz-huge file.
+Make sure that the vmlinuz file there is replaced by the vmlinuz-generic file.
 
 Next, re-create the initrd Run this command, inserting the version number of
 the new kernel:
 
 	# /usr/share/mkinitrd/mkinitrd_command_generator.sh -k 4.4.75
 
-It should echo back a command including a big list of kernel modules
-MESSAGE
+It should echo back a command including a big list of kernel modules"
+
+case $HOSTNAME in
+	voyager2*|mariner*)
+		if [[ -n $last_update ]]; then
+			if sed -n -e '/a\/kernel-.*/q0' -e "/^$last_update/q1" /var/lib/slackpkg/ChangeLog.txt; then
+				print $IMPORTANT
+				_TODO+=(
+				'$ cd /boot/efi/EFI/Slackware/'
+				'copy the new vmlinuz* files from /boot to here (not symlinks)'
+				'replace the vmlinuz file with vmlinuz-generic'
+				'run "/usr/share/mkinitrd/mkinitrd_command_generator.sh -k <kernel ver>"'
+				'copy the resulting /boot/initrd.gz up here'
+				)
+			fi
+		else
+			print $IMPORTANT
+		fi
+		;;
+esac
+
 }
 
 source $0:h/__TASKS.zsh
