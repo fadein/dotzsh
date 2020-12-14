62143c6c47baf279f5aa5b2b3bde5889c0aebaaa
Erik at School <erik.falor@usu.edu>
2020-12-14 13:34:34 -0700

    gitlabupgrade task: check existence and perms of custom log files

--- tasks/gitlabupgrade.zsh
+++ tasks/gitlabupgrade.zsh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 
  PURPOSE="GitLab server update task"
- VERSION="1.3"
-    DATE="Sun Aug 30 20:31:14 MDT 2020"
+ VERSION="1.4"
+    DATE="Mon Dec 14 13:34:11 MST 2020"
   AUTHOR="Erik Falor"
 PROGNAME=$0
 TASKNAME=$0:t:r
@@ -31,6 +31,7 @@ env() {
 		"Make sure the hook was replaced by pushing a commit"
 		"Retire the webpage broadcast message"
 		'$ cd; mv gitlab.msg gitlab.msg.old'
+        'Make sure that /var/log/gitlab/{errors,huge_repos}.log exist, are 0644 and "git:git"'
 		'$ if [[ -f /var/run/reboot-required ]]; then print Reboot is required; else print Reboot is NOT required; fi'
 	)
 }
