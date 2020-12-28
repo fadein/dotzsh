31ece990782c48823579e808f0f782149e4be5d9
Erik on Columbia <erik.falor@usu.edu>
2020-12-28 12:50:22 -0700

    columbia prompt color

--- 50-prompts.zsh
+++ 50-prompts.zsh
@@ -19,6 +19,8 @@ if ! functions hostcolor >/dev/null; then
                     function hostcolor { print "%B%F{magenta}$1%f%b"; } ;;
                 endeavour*)
                     function hostcolor { print "%F{red}$1%f"; } ;;
+                columbia*)
+                    function hostcolor { print "%B%F{yellow}$1%f%b"; } ;;
                 mariner)
                     function hostcolor { print "%B%F{cyan}%K{black}$1%k%f%b"; } ;;
                 *)
