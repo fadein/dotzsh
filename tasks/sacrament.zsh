aac34f9d8ef76d114819f14c4acaf73ad788fbdb
Erik on Apollo <ewfalor@gmail.com>
2022-02-25 17:32:58 -0700

    sundry updates to recycle(), sacrament.zsh

--- tasks/sacrament.zsh
+++ tasks/sacrament.zsh
@@ -1,8 +1,8 @@
 #!/usr/bin/env zsh
 
 PURPOSE="Broadcast sacrament meeting"
-VERSION="1.4.1"
-   DATE="Sun Jul 11 15:55:49 MDT 2021"
+VERSION="1.6"
+   DATE="Sun 30 Jan 2022 08:39:41 AM MST"
  AUTHOR="Erik"
 
 PROGNAME=$0
@@ -31,10 +31,6 @@ env() {
 	reminder() {
 		>&1 <<-MESSAGE | pms numberedList | pms colors | pms centerVertical
 
-		Visit the Liahona network login page @ http://10.1.10.28
-
-		Disable the Wireless Access points in $TM
-
 		Zoom takes several minutes to settle down upon startup.
 
 		Enter the meeting several minutes early so the computer settles down
@@ -42,22 +38,12 @@ env() {
 
 		Rename myself to "Riverside Ward"
 
-		Go into the security tab and disable
-			*	users can unmute themselves
-			*	users can chat
-			*	users can share their screen
-
-		Go into the participants tab and mute everybody
-			*	mute all current and new participants
-
-		Right click my video pane and "Spotlight Video"
 		MESSAGE
 	}
 
-	localc ~/Documents/Church/2021Q1_SacramentMtgPrelude.pptx >/dev/null 2>&1 & disown
+	localc ~/Documents/Church/2022Jan29_SacramentMtgPrelude.pptx >/dev/null 2>&1 & disown
 
     reminder
-    firefox $TM https://usu-edu.zoom.us & disown
 }
 
 cleanup() {
