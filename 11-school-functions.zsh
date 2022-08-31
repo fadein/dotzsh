99c299843eb296c2259692428795792dd850709e
Erik on Mariner <erik.falor@usu.edu>
2022-08-31 17:15:20 -0600

    11-school-functions.zsh refresh(): be more verbose so I can debug merge conflicts

--- 11-school-functions.zsh
+++ 11-school-functions.zsh
@@ -2,6 +2,19 @@
 SEMESTYR=Fa22
 BASE=/home/fadein/school
 
+# Show a command and then run it
+# When the environment variable DRYRUN is non-empty, do not
+# actually make any changes, but only show what would be done
+echodo() {
+	print -P "\n%B%~ %% %b%F{green}$@%f"
+	if [[ -z $DRYRUN ]]; then
+		$@
+	else
+		true
+	fi
+}
+
+
 refresh() {
     for REPO in $BASE/$SEMESTYR/*/.git; do
         (
@@ -13,25 +26,33 @@ refresh() {
 
             # put myself back on the master branch so the pull will cleanly
             # apply if/when I'm working on a topic branch
-            git checkout master
-            git pull --recurse-submodules=on-demand
+            echodo git checkout master
+            echodo git pull --recurse-submodules=on-demand
             (
-                cd Notes
-                git checkout master
-                if ! git pull publish master && git pull origin master; then
-                    print
-                    print "Pulling Notes/ failed; You must manually address this"
-                    print
+                echodo cd Notes
+                echodo git checkout master
+
+                if ! echodo git pull publish master; then
+                    print -P "\n%F{yellow}%BPulling%b%f Notes/ from %F{green}%Bpublish%b%f failed; You must manually address this\n"
                     return 1
                 fi
-                if ! git push origin master && git push publish master; then
-                    print
-                    print "Pushing Notes/ failed; You must manually address this"
-                    print
+
+                if ! echodo git pull origin master; then
+                    print -P "\n%F{yellow}%BPulling%b%f Notes/ from %F%B{cyan}origin%b%f failed; You must manually address this\n"
+                    return 1
+                fi
+
+                if ! echodo git push publish master; then
+                    print "\n%F{red}%BPushing%b%f Notes/ to %F{green}%Bpublish%b%f failed; You must manually address this\n"
+                    return 1
+                fi
+
+                if ! echodo git push origin master; then
+                    print "\n%F{red}%BPushing%b%f Notes/ to %F{cyan}%Borigin%b%f failed; You must manually address this\n"
                     return 1
                 fi
             )
-            git push
+            echodo git push origin master
             print
         )
     done
