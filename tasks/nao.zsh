c538e4ebb7232460a8fec1df46ed8fe184698615
Erik on Endeavour <erik.falor@usu.edu>
2023-10-17 10:02:38 -0600

    tasks/nao.zsh: finds the right font size before playing

--- tasks/nao.zsh
+++ tasks/nao.zsh
@@ -1,21 +1,22 @@
 #!/bin/env zsh
 
 PURPOSE="Play on nethack.alt.org or hardfought.org"
-VERSION="3.0"
-   DATE="Sun Oct 15 20:52:27 MDT 2023"
+VERSION="3.1"
+   DATE="Tue Oct 17 09:47:33 MDT 2023"
  AUTHOR="erik"
 
 PROGNAME=$0
 TASKNAME=$0:t:r
 
 
-declare -a LC  # [columns lines]
+declare -a cols_lines  # [columns lines]
 
 # use tput(1) to find the dimensions of the terminal
-# save in the global array LC
+# save in the global array cols_lines
 get-size() {
-    LC=($(tput cols lines))
+    cols_lines=($(tput cols lines))
 }
+
 
 countdown() {
     if [[ $# -lt 1 ]]; then
@@ -49,56 +50,62 @@ countdown() {
 nethack-right-size() {
     VERSION="1.1"
 
-    MAX_SIZE=76
-    MIN_SIZE=12
-    STEP=2
+    declare -r max=76
+    declare -r min=12
+    declare -r step=2
 
-    declare -a TARGET_SIZE=(130 36)
-    declare -a ORIG_SIZE  # [columns lines]
-    local ORIG_FONT_SIZE=
-    local GOOD=
+    declare -a target_size=(131 36)  # ( columns lines )
+    declare -a orig_size             # ( columns lines )
+    local orig_font_size=
+    local good=
 
     # get the current size of the screen so I can reset back to the original size
     # if we fail to find a good match
     get-size
-    ORIG_SIZE=(${LC[@]})
+    if (( ${cols_lines[1]} == ${target_size[1]} && ${cols_lines[2]} == ${target_size[2]} )); then
+        print "${cols_lines[1]} == ${target_size[1]} && ${cols_lines[2]} == ${target_size[2]}"
+        print "This font size is just right..."
 
+        return
+    fi
+
+    orig_size=(${cols_lines[@]})
 
     # Starting at the largest font, reduce the size of font by
-    # increments of STEP until both of the target dimensions are met
-    for ((i = MAX_SIZE; i >= MIN_SIZE; i -= STEP )); do
+    # increments of $step until both of the target dimensions are met
+    for ((i = max; i >= min; i -= step )); do
         print "\033]710;xft:hack:pixelsize=${i}:antialias=true\007"
         # print "[H[2JTrying font size $i..."
         print "Trying font size $i..."
         sleep .15
         get-size
-        print "At font size $i dimensions are ${LC[1]}x${LC[2]}"
+        print "At font size $i dimensions are ${cols_lines[1]}x${cols_lines[2]}"
 
-        if (( ${LC[1]} == ${ORIG_SIZE[1]} && ${LC[2]} == ${ORIG_SIZE[2]} )); then
-            ORIG_FONT_SIZE=$i
+        if (( ${cols_lines[1]} == ${orig_size[1]} && ${cols_lines[2]} == ${orig_size[2]} )); then
+            orig_font_size=$i
             print "The original font size was $i"
         fi
 
-        if (( ${LC[1]} >= ${TARGET_SIZE[1]} && ${LC[2]} >= ${TARGET_SIZE[2]} )); then
-            print "${LC[1]} >= ${TARGET_SIZE[1]} && ${LC[2]} >= ${TARGET_SIZE[2]}"
+        if (( ${cols_lines[1]} >= ${target_size[1]} && ${cols_lines[2]} >= ${target_size[2]} )); then
+            print "${cols_lines[1]} >= ${target_size[1]} && ${cols_lines[2]} >= ${target_size[2]}"
             print "Font size $i is just right..."
-            GOOD=1
+            good=1
             break
         fi
     done
 
-    if [[ -z $GOOD && -n $ORIG_FONT_SIZE ]]; then
+    if [[ -z $good && -n $orig_font_size ]]; then
         print "Appropriate font size not found; restoring to original size"
-        print "\033]710;xft:hack:pixelsize=${ORIG_FONT_SIZE}:antialias=true\007"
+        print "\033]710;xft:hack:pixelsize=${orig_font_size}:antialias=true\007"
 
-    elif [[ -z $GOOD && -z $ORIG_FONT_SIZE ]]; then
+    elif [[ -z $good && -z $orig_font_size ]]; then
         print "Failed to find a good font size and I don't know what size to reset to. Sorry!"
 
-    elif [[ -n $GOOD ]]; then
+    elif [[ -n $good ]]; then
         return 0
 
     else
-        print "Never found a good font size"
+        print "Failed to find a good font size"
         return 1
 
     fi
@@ -109,17 +116,19 @@ setup() {
     if nethack-right-size; then
         setxkbmap us,colehack -option grp:ctrls_toggle -option grp_led:scroll
         clear
-        countdown 3 print Entering the Dungeons of Doom from ${TTY:t2}...
+        print Entering the Dungeons of Doom on ${TTY:t2} in
+        countdown 5
     else
         return 1
     fi
 }
 
 spawn() {
-        case $TASKNAME in
-            nao) ssh nethack@alt.org ;;
-            hardfought) ssh nethack@hardfought.org ;;
-        esac
+    case $TASKNAME in
+        nao) ssh nethack@alt.org ;;
+        hardfought) ssh nethack@hardfought.org ;;
+        nethack) MAILREADER=/usr/bin/mutt command nethack ;;
+    esac
 }
 
 cleanup() {
