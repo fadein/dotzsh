cdc95f228ef3ad46cfef6112fa0d07e301d36108
Erik on Voyager2 <erik.falor@usu.edu>
2024-03-31 23:41:54 -0600

    sudo ZLE widget: press Esc 2x to toggle `sudo` command modifier

--- 35-bindings.zsh
+++ 35-bindings.zsh
@@ -13,3 +13,29 @@ bindkey -s "\es" "\C-acsi -p '\C-e'\C-b"
 # Remove binding to Ctrl-D (default is delete-char-or-list)
 # Aesthetic improvement in lectures where stty(1) disables Ctrl-D's EOF meaning
 bindkey -r "\C-D"
+
+
+# A user-defined ZLE widget that toggles `sudo` or `sudoedit` in front
+# of the current (or previous) command
+sudo-command-line() {
+    [[ -z $BUFFER ]] && zle up-history
+    if [[ $BUFFER == sudo\ * ]]; then
+        LBUFFER="${LBUFFER#sudo }"
+    elif [[ $BUFFER == $EDITOR:t\ * ]]; then
+        LBUFFER="${LBUFFER#$EDITOR:t }"
+        LBUFFER="sudoedit $LBUFFER"
+    elif [[ $BUFFER == $EDITOR\ * ]]; then
+        LBUFFER="${LBUFFER#$EDITOR }"
+        LBUFFER="sudoedit $LBUFFER"
+    elif [[ $BUFFER == sudoedit\ * ]]; then
+        LBUFFER="${LBUFFER#sudoedit }"
+        LBUFFER="$EDITOR:t $LBUFFER"
+    else
+        LBUFFER="sudo $LBUFFER"
+    fi
+}
+zle -N sudo-command-line
+
+# Bind this widget to the keys: [Esc] [Esc]
+bindkey "^[^[" sudo-command-line
+# bindkey -M vicmd "^[^[" sudo-command-line  # uncomment for vi-mode
