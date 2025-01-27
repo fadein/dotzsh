0b756d732e86dbc04a72ea9490c8706c9b3f99b1
Erik on Atlantis <erik.falor@usu.edu>
2025-01-26 23:52:41 -0700

    1080p.zsh works on atlantis

--- tasks/1080p.zsh
+++ tasks/1080p.zsh
@@ -1,25 +1,36 @@
 #!/bin/env zsh
 
 PURPOSE="enter 1080p mode"
-VERSION="1.2"
-   DATE="Sun Oct  1 14:13:26 MDT 2023"
+VERSION="1.3"
+   DATE="Sun Jan 26 2025"
  AUTHOR="fadein"
 
 PROGNAME=$0
 TASKNAME=$0:t:r
 
 
+case $HOSTNAME in
+    atlantis)
+        display=eDP
+        resolution=2880x1920
+        ;;
+    columbia|endeavour)
+        display=eDP-1 
+        resolution=3840x2160
+        ;;
+esac
+
 setup() {
     # Set the DPI for Firefox
     echo Xft.dpi: 132 | xrdb -quiet -override
-    xrandr --output eDP-1 --mode 1920x1080
+    xrandr --output $display --mode 1920x1080
     pkill -SIGUSR1 conky
     CLEANUP_TRAPS=(HUP)
 }
 
 cleanup() {
     echo Xft.dpi: 220 | xrdb -quiet -override
-    xrandr --output eDP-1 --mode 3840x2160 --auto
+    xrandr --output $display --mode $resolution --auto
     pkill -SIGUSR1 conky
 }
 
