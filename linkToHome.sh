0c4549cc187d8f0c3f967713958c2fa430100a30
Erik on Voyager2 <erik.falor@usu.edu>
2025-03-27 18:43:06 -0600

    linkToHome.sh - failed to locate itself

--- linkToHome.sh
+++ linkToHome.sh
@@ -78,7 +78,7 @@ if [ 0"$1" = 0"-r" ]; then
 	removeLink .zshenv
 else
 	# Resolve the location of this script
-	HERE=$(pwd)
+    HERE=$(dirname $(readlink -f $0))
 
 	# Link these files and directories into $HOME
 	linkToHome $HERE               .zsh
