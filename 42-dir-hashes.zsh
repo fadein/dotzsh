5a47ced7fa03937f01295f820c1d098efc36cb53
Erik on Mariner <erik.falor@usu.edu>
2025-03-27 18:07:32 -0600

    directory hashes

---
+++ 42-dir-hashes.zsh
@@ -0,0 +1,14 @@
+#
+# Hashed directory shortcuts
+#
+# To use, prepend a ~ to their name. Ex:
+#   cd ~vlp
+#
+hash -d vlp=/var/log/packages/
+hash -d vl=/var/log/
+hash -d ssh=/etc/ssh/
+hash -d rc.d=/etc/rcd/
+hash -d slackpkg=/etc/slackpkg/
+hash -d cups=/etc/cups/
+hash -d x11=/etc/X11/
+hash -d nm=/etc/NetworkManager/system-connections/
