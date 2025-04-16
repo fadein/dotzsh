4c22368c9dfc69c28fe129f780d34a57a46dd983
Erik on Atlantis <erik.falor@usu.edu>
2025-04-15 19:25:02 -0600

    "commit" == "git show"; reorganize aliases + add a Vim sort command

--- 50-aliases.zsh
+++ 50-aliases.zsh
@@ -1,20 +1,24 @@
 #
 # User-specific aliases
-#
-alias clipi="xclip -sel clip -i"
-alias clipin="xclip -sel clip -i"
-alias clipo="xclip -sel clip -o"
-alias clipout="xclip -sel clip -o"
+# +,/^$/-1sort
+alias clipi='xclip -sel clip -i'
+alias clipin='xclip -sel clip -i'
+alias clipo='xclip -sel clip -o'
+alias clipout='xclip -sel clip -o'
+alias commit='git show'
+alias copy='cp -i'
 alias cp='cp -i'
 alias csi='csi -wq'
 alias ctags='ctags --fields=+iaS --extra=+fq'
-alias curl='curl -A "Mozilla/4.0"'
 alias date='date +"%a, %b %e %Y  %R %Z"'
+alias del='rm -i'
+alias deltree='rm -rf'
 alias df='df -h'
 alias diff='diff --color=auto'
 alias diffu='diff --color=auto -u'
-alias dmesg="dmesg -H"
+alias dmesg='dmesg -H'
 alias du='du -h'
+alias epoch='strftime %c'
 alias feh='feh --scale-down'
 alias ffmpeg='ffmpeg -hide_banner'
 alias ffplay='ffplay -hide_banner'
@@ -22,17 +26,17 @@ alias ffprobe='ffprobe -hide_banner'
 alias findd='find . -type d -name'
 alias findf='find . -type f -name'
 alias free='free -m'
+alias gcc-defines='gcc -dM -E -x c - </dev/null | sort'
 alias gdb='gdb -q'
-alias gi=git
-alias gitc=git
 alias grep='grep -n --color=auto'
 alias grepi='grep -i -n --color=auto'
 alias grev='grep -vn --color=auto'
-alias ip="ip -color=auto"
+alias ip='ip -color=auto'
 alias lolcat='lolcat -v .85'
 alias lynx='lynx -use_mouse'
 alias mcp='mcp -i'
 alias mmv='mmv -i'
+alias move='mv -i'
 alias mplayer='mplayer -af scaletempo'
 alias mv='mv -i'
 alias niceme='renice -n 10 -p $$'
@@ -44,10 +48,86 @@ alias pseudo=sudo
 alias pwd='pwd -P'
 alias rl=readlink
 alias rm='rm -i'
-alias topu="top -u $USER"
+alias screen-r='screen -r'
+alias screenr='screen -r'
+alias screern='screen -r'
+alias topu="htop -u $USER"
 alias vimdiff='vim -d'
-alias which="which -p"
+alias which='which -p'
 
+#
+# because I can't spell...
+# +,/^$/-1sort
+alias cdx=cd
+alias cs=cd
+alias ecoh=echo
+alias ehco=echo
+alias fiel=file
+alias gerp=grep
+alias gitlog='git log'
+alias got=git
+alias grpe=grep
+alias gti=git alias igt=git
+alias ivm=vim
+alias jbos=jobs
+alias gi=git
+alias gitc=git
+alias jods=jobs
+alias jorbs=jobs
+alias les=less
+alias lss=less
+alias maek=make
+alias mak=make
+alias mor=more
+alias mplayre=mplayer
+alias mroe=more
+alias pgerp='pgrep -l'
+alias pyhton=python
+alias scd=cd
+alias sl=ls
+alias sls=ls
+alias vm=vim
+alias vmi=vim
+
+#
+# vi commands
+# +,/^$/-1sort
+alias /=ack
+alias :close='echo E444: Cannot close last window'
+alias :e=vim
+alias :ex=vim
+alias :ls=ls
+alias :q!=exit
+alias :q=exit
+alias :qa=exit
+alias :r=cat
+alias :r=less
+alias :split=vim
+alias :w!='echo E503: is not a file or writable device'
+alias :w='echo "E505: is read-only (add ! to override)"'
+alias :wq=exit
+alias :x!=exit
+alias :x=exit
+alias ZQ=exit
+alias ZZ=exit
+
+#
+# suffix aliases
+# +,/^$/-1sort
+alias -s com=lynx
+alias -s conf=vim
+alias -s doc=lowriter
+alias -s docx=lowriter
+alias -s net=lynx
+alias -s odp=loimpress
+alias -s ods=localc
+alias -s odt=lowriter
+alias -s org=lynx
+alias -s ppt=loimpress
+alias -s pptx=loimpress
+alias -s txt=vim
+alias -s xls=localc
+alias -s xlsx=localc
 
 #
 # OS-specific
@@ -77,88 +157,3 @@ case $OSTYPE in
         ;;
 esac
 
-#
-# because I can't spell...
-#
-alias cdx=cd
-alias cs=cd
-alias ecoh=echo
-alias ehco=echo
-alias fiel=file
-alias gerp=grep
-alias gitlog='git log'
-alias got=git
-alias grpe=grep
-alias gti=git
-alias igt=git
-alias ivm=vim
-alias jbos=jobs
-alias jods=jobs
-alias jorbs=jobs
-alias les=less
-alias lss=less
-alias maek=make
-alias mak=make
-alias mor=more
-alias mplayre=mplayer
-alias mroe=more
-alias pgerp='pgrep -l'
-alias pyhton=python
-alias scd=cd
-alias sl=ls
-alias sls=ls
-alias vm=vim
-alias vmi=vim
-
-#
-# vi commands
-#
-alias /=ack
-alias :close='echo E444: Cannot close last window'
-alias :e=vim
-alias :ex=vim
-alias :ls=ls
-alias :q!=exit
-alias :q=exit
-alias :qa=exit
-alias :r=cat
-alias :r=less
-alias :split=vim
-alias :w!='echo E503: is not a file or writable device'
-alias :w='echo "E505: is read-only (add ! to override)"'
-alias :wq=exit
-alias :x!=exit
-alias :x=exit
-alias ZQ=exit
-alias ZZ=exit
-
-#
-# 1337 h4X0r 4L14535
-#
-alias copy='cp -i'
-alias del='rm -i'
-alias deltree='rm -rf'
-alias epoch='strftime %c'
-alias gcc-defines='gcc -dM -E -x c - </dev/null | sort'
-alias move='mv -i'
-alias screen-r='screen -r'
-alias screenr='screen -r'
-alias screern='screen -r'
-
-#
-# suffix aliases
-#
-alias -s com=lynx
-alias -s conf=vim
-alias -s doc=lowriter
-alias -s docx=lowriter
-alias -s net=lynx
-alias -s odp=loimpress
-alias -s ods=localc
-alias -s odt=lowriter
-alias -s org=lynx
-alias -s ppt=loimpress
-alias -s pptx=loimpress
-alias -s txt=vim
-alias -s xls=localc
-alias -s xlsx=localc
