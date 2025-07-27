8a6b8a12bd03a7ebca8133c1800e78301b9daef9
Erik on Atlantis <erik.falor@usu.edu>
2025-07-27 09:49:01 -0600

    alias: htopu

--- 50-aliases.zsh
+++ 50-aliases.zsh
@@ -38,6 +38,7 @@ alias gdb='gdb -q'
 alias grep='grep -n --color=auto'
 alias grepi='grep -i -n --color=auto'
 alias grev='grep -vn --color=auto'
+alias htopu="htop -u $USER"
 alias ip='ip -color=auto'
 alias lolcat='lolcat -v .85'
 alias lynx='lynx -use_mouse'
