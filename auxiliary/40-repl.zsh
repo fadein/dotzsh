d012fa2acfd3bf9ae6f0de605547960519120dce
Erik on Atlantis <erik.falor@usu.edu>
2025-09-09 17:19:08 -0600

    repl() function

---
+++ auxiliary/40-repl.zsh
@@ -0,0 +1,17 @@
+repl() {
+    if (( $# == 0 )); then
+        1>&2 echo "Load a Python script into the REPL"
+        1>&2 echo "Run exit() to restart the REPL, exit(1) to quit to the shell"
+        1>&2 echo "    Usage: repl SCRIPT.py"
+        return 1
+    elif [[ ! -e $1 ]]; then
+        1>&2 echo "File '$1' does not exist"
+        1>&2 echo "    Usage: repl SCRIPT.py"
+        return 1
+    fi
+
+    while python -i $1; do
+        echo "run exit() to restart, exit(1) to quit"
+        sleep .4
+    done
+}
