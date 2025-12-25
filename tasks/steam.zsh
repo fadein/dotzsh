c74172571aaba11413b1b2cc9e609dc271c7e809
Erik on Atlantis <erik.falor@usu.edu>
2025-12-24 18:43:41 -0700

    steam.zsh: Run Steam through Conty

---
+++ tasks/steam.zsh
@@ -0,0 +1,39 @@
+#!/bin/env zsh
+
+PURPOSE="Run Steam through Conty"
+VERSION="1.0"
+   DATE="Wed Dec 17 2025"
+ AUTHOR="fadein"
+
+PROGNAME=$0
+TASKNAME=$0:t:r
+
+setup() {
+	if   command -v conty_lite_dwarfs.sh >/dev/null; then
+		CONTY=conty_lite_dwarfs.sh
+	elif command -v conty_lite.sh >/dev/null; then
+		CONTY=conty_lite.sh
+	elif command -v conty.sh >/dev/null; then
+		CONTY=conty.sh
+	else
+		print 'Failed to locate a conty*.sh in $PATH; exiting'
+		exit 1
+	fi
+
+	export CONTY
+	killall picom
+}
+
+env() {
+	$CONTY steam & disown
+	figlet -t "Exit this shell to terminate steam"
+}
+
+cleanup() {
+	killall steam
+	picom & disown
+}
+
+source $0:h/__TASKS.zsh
+
+# vim:set foldenable foldmethod=indent filetype=zsh tabstop=4 shiftwidth=4 noexpandtab:
