c38153a011db4b0426b328ce3cfa9cc2a586320b
Erik on Atlantis <erik.falor@usu.edu>
2026-01-16 16:47:09 -0700

    improved pyenv auxilliary script

--- auxiliary/93-pyenv.zsh
+++ auxiliary/93-pyenv.zsh
@@ -1,4 +1,8 @@
 # pyenv setup
-path=( ~/.pyenv/bin $path )
-export PYENV_ROOT=~/.pyenv
-eval "$(pyenv init -)"
+if [[ -d ~/.pyenv/bin ]]; then
+    export PYENV_ROOT=~/.pyenv  # where Pyenv's files live
+    [[ -d $PYENV_ROOT/bin ]] && path=( $PYENV_ROOT/bin $path )  # add Pyenv to path
+    eval "$(pyenv init - zsh)"  # add Pyenv functions to shell
+else
+    1>&2 print "Pyenv home not found under ~/.pyenv"
+fi
