bb53d75d98677d458acb42801e672541cdcc0509
Erik on Atlantis <erik.falor@usu.edu>
2026-01-16 16:47:21 -0700

    Initialize the SSH Agent in an anonymous function (for clean scope)

--- auxiliary/ssh-agent.zsh
+++ auxiliary/ssh-agent.zsh
@@ -1,6 +1,20 @@
-# Initialize the SSH Agent
+# Initialize the SSH Agent in an anonymous function (for clean scope)
 
-if [[ -d ~/devel/shell/ssh-agent ]]; then
-    path+=(~/devel/shell/ssh-agent)
-    source ssh-agent-startup.sh
-fi
+function {
+    # print "Initializing the SSH Agent"
+    local dir dirs=(~/devel/ssh-agent ~/devel/shell/ssh-agent ~/.local/ssh-agent ~/.ssh-agent FAILED-TO-FIND-SSH-AGENT)
+    for dir in $dirs; do
+        # print Trying $dir
+        if [[ -d $dir ]]; then
+            path+=($dir)
+            source ssh-agent-startup.sh
+            break
+        fi
+    done
+
+    # if [[ $dir == FAILED-TO-FIND-SSH-AGENT ]]; then
+    #     print SSH Agent was not initialized
+    # else
+    #     print SSH Agent was initialized
+    # fi
+}
